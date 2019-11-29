642f98375a8edec71d27b3374ede9e41545bce4f
user <info@apocalyptiarpg.com>
2019-11-29 15:47:00 -0500

    finished MeleeList

--- src/data/gear/melee.js
+++ src/data/gear/melee.js
@@ -1,49 +1,58 @@
-function MeleeWeapon(type, dmg, hands, notes, sz){
-    this.type = type;
-    this.dmg = dmg;
-    this.hands = hands;
-    this.notes = notes;
-    this.sz = sz;
+class MeleeWeapon {
+    constuctor(type, dmg, hands, rng, notes, sz) {
+        this.type = type
+        this.dmg = dmg
+        this.hands = hands
+        this.rng = rng
+        this.notes = notes
+        this.sz = sz
+    }
 }
-const iAx = new MeleeWeapon('Ax', 5, 2, 'Chop.', 4);
-const iBaseballBat = new MeleeWeapon('Baseball Bat', 3, 2, 'Blunt.', 3);
-const iBrassKnuckles = new MeleeWeapon('Brass Knuckles', 2, 1, 'Blunt.', 1);
-const iBaton = new MeleeWeapon('Baton', 2, 1, 'Blunt. Rapid.', 2);
-const iCane = new MeleeWeapon('Cane', 1, 1, 'Blunt. +1 Trip. Can be used as a Crutch.', 3);
-const iCleaver = new MeleeWeapon('Cleaver', 2, 1, 'Chop.', 1);
-const iCrowbar = new MeleeWeapon('Crowbar', 3, 1, 'Lever.', 3);
-const iFirepoker = new MeleeWeapon('Firepoker', 3, 1, 'Lever. Pierce.', 3);
-const iGarrote = new MeleeWeapon('Garrote', 1, 2, 'Blunt. +3 to Grab(Lock) Head.', 1);
-const iHammer = new MeleeWeapon('Hammer', 2, 1, 'Lever.', 2);
-const iHatchet = new MeleeWeapon('Hatchet', 2, 1, 'Chop.', 2);
-const iKnife = new MeleeWeapon('Knife', 2, 1, 'Rapid.', 1);
-const iMachete = new MeleeWeapon('Machete', 3, 1, 'Chop.', 2);
-const iMetalClub = new MeleeWeapon('Metal Club', 3, 2, 'Blunt.', 3);
-const iPickax = new MeleeWeapon('Pickax', 6, 2, 'Lever. Pierce.', 5);
-const iPitchfork = new MeleeWeapon('Pitchfork', 3, 2, '+1 Block. Pierce.', 4);
-const iRiotShield = new MeleeWeapon('Riot Shield', 1, 1, '+3 Block. Blunt. Cover 3DR.', 4);
-const iScrewdriver = new MeleeWeapon('Screwdriver', 1, 1, 'Lever. Pierce. Rapid.', 1);
-const iShovel = new MeleeWeapon('Shovel', 3, 2, '+1 Block', 4);
-const iSledgehammer = new MeleeWeapon('Sledgehammer', 5, 2, 'Blunt.', 5);
-const iSpear = new MeleeWeapon('Spear', 4, 2, '+1 Block. Pierce.', 3);
-const iStaff = new MeleeWeapon('Staff', 2, 2, '+1 Block. Blunt.', 3);
-const iTireIron = new MeleeWeapon('Tire Iron', 2, 1, 'Lever.', 2);
-const iTorch = new MeleeWeapon('Torch', 1, 1, 'Blunt. +1 FDMG. 5yd light radius 1hr.', 2);
 
-    // RARE
-    // const iBarbwireClub = new MeleeWeapon('Barbwire Club', 3, 1, '', 2);
-    // const iBowieKnife = new MeleeWeapon('Bowie Knife', 2, 1, 'Chop. Rapid.', 1);
-    // const iBroadsword = new MeleeWeapon('Broadsword', 4, 2, 'Chop or Pierce.', 4);
-    // const iCatchPole = new MeleeWeapon('Catch Pole', 0, 2, '+1 Block. Blunt. +1 Grab.', 3);
-    // const iChainsaw = new MeleeWeapon('Chainsaw', 6, 2, '.5gal Fuel. d6rnd start. 1: Empty. Loud.', 4);
-    // const iIceAx = new MeleeWeapon('Ice Ax', 3, 1, 'Lever. Pierce.', 2);
-    // const iKatana = new MeleeWeapon('Katana', 5, 2, 'Chop or Pierce. Rapid.', 3);
-    // const iKukri = new MeleeWeapon('Kukri', 3, 1, 'Chop', 2);
-    // const iLasso = new MeleeWeapon('Lasso', 0, 2, 'Blunt. +1 Grab. Throw (RNG:3)', 2);
-    // const iNet = new MeleeWeapon('Net', 0, 2, '+6 Grab.', 3);
-    // const iRapier = new MeleeWeapon('Rapier', 3, 1, 'Pierce. Rapid.', 2);
-    // const iScythe = new MeleeWeapon('Scythe', 6, 2, 'Chop. Pierce.', 4);
-    // const iSignShield = new MeleeWeapon('Sign Shield', 2, 1, '+3 Block. Cover 6DR.', 4);
-    // const iSwitchblade = new MeleeWeapon('Switchblade', 1, 1, 'Pierce. Rapid.', 0);
-    // const iTrenchKnife = new MeleeWeapon('Trench Knife', 2, 1, 'Blunt Punch. Pierce. Rapid.', 1);
-    // const iWhip = new MeleeWeapon('Whip', 0, 1, 'Blunt. +1 Disarm. +1 Grab. RNG:3.', 1);
+export const MeleeList = [
+    new MeleeWeapon('Ax',               5, 2, 2, ['Slow', 'Chop'],          4),
+    new MeleeWeapon('Baseball Bat',     3, 2, 2, [],                        3),
+    new MeleeWeapon('Brass Knuckles',   2, 1, 1, ['Unarmed'],               1),
+    new MeleeWeapon('Crowbar',          3, 1, 2, [],                        3),
+    new MeleeWeapon('Hammer',           2, 1, 1, [],                        2),
+    new MeleeWeapon('Hatchet',          2, 1, 1, ['Chop'],                  2),
+    new MeleeWeapon('Knife',            2, 1, 1, ['Pierce', 'Rapid'],       1),
+    new MeleeWeapon('Machete',          3, 1, 2, ['Chop'],                  2),
+    new MeleeWeapon('Shield',           1, 1, 1, ['Cover 3DR', '+3 Block'], 4),
+    new MeleeWeapon('Sledgehammer',     5, 2, 2, ['Slow'],                  5),
+    new MeleeWeapon('Spear',            4, 2, 3, ['Pierce'],                3),
+    new MeleeWeapon('Staff',            2, 2, 3, [],                        3),
+    
+]
+
+// RARE
+// new MeleeWeapon('Barbwire Club', 3, 1, '', 2),
+// new MeleeWeapon('Bowie Knife', 2, 1, 'Chop. Rapid.', 1),
+// new MeleeWeapon('Broadsword', 4, 2, 'Chop or Pierce.', 4),
+// new MeleeWeapon('Catch Pole', 0, 2, '+1 Block. Blunt. +1 Grab.', 3),
+// new MeleeWeapon('Chainsaw', 6, 2, '.5gal Fuel. d6rnd start. 1: Empty. Loud.', 4),
+// new MeleeWeapon('Ice Ax', 3, 1, 'Lever. Pierce.', 2),
+// new MeleeWeapon('Katana', 5, 2, 'Chop or Pierce. Rapid.', 3),
+// new MeleeWeapon('Kukri', 3, 1, 'Chop', 2),
+// new MeleeWeapon('Lasso', 0, 2, 'Blunt. +1 Grab. Throw (RNG:3)', 2),
+// new MeleeWeapon('Net', 0, 2, '+6 Grab.', 3),
+// new MeleeWeapon('Rapier', 3, 1, 'Pierce. Rapid.', 2),
+// new MeleeWeapon('Scythe', 6, 2, 'Chop. Pierce.', 4),
+// new MeleeWeapon('Sign Shield', 2, 1, '+3 Block. Cover 6DR.', 4),
+// new MeleeWeapon('Switchblade', 1, 1, 'Pierce. Rapid.', 0),
+// new MeleeWeapon('Trench Knife', 2, 1, 'Blunt Punch. Pierce. Rapid.', 1),
+// new MeleeWeapon('Whip', 0, 1, 'Blunt. +1 Disarm. +1 Grab. RNG:3.', 1),
+
+// OLD
+// new MeleeWeapon('Baton', 2, 1, 'Blunt. Rapid.', 2),
+// new MeleeWeapon('Cane', 1, 1, 'Blunt. +1 Trip. Can be used as a Crutch.', 3),
+// new MeleeWeapon('Cleaver', 2, 1, 'Chop.', 1),
+// new MeleeWeapon('Firepoker', 3, 1, 'Lever. Pierce.', 3),
+// new MeleeWeapon('Garrote', 1, 2, 'Blunt. +3 to Grab(Lock) Head.', 1),
+// new MeleeWeapon('Metal Club', 3, 2, 'Blunt.', 3),
+// new MeleeWeapon('Pickax', 6, 2, 'Lever. Pierce.', 5),
+// new MeleeWeapon('Pitchfork', 3, 2, '+1 Block. Pierce.', 4),
+// new MeleeWeapon('Screwdriver', 1, 1, 'Lever. Pierce. Rapid.', 1),
+// new MeleeWeapon('Shovel', 3, 2, '+1 Block', 4),
+// new MeleeWeapon('Tire Iron', 2, 1, 'Lever.', 2),
+// new MeleeWeapon('Torch', 1, 1, 'Blunt. +1 FDMG. 5yd light radius 1hr.', 2),
