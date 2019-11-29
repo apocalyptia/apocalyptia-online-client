dd20d8b07ffe1f4b9ab6ac16e320695cce2d6977
user <info@apocalyptiarpg.com>
2019-11-29 14:25:42 -0500

    finished DrugList

--- src/data/gear/drugs.js
+++ src/data/gear/drugs.js
@@ -1,20 +1,26 @@
-
-       //DRUGS
-    function Drug(type, notes, sz){
-        this.type = type;
-        this.notes = notes;
-        this.sz = sz;
+class Drug {
+    constuctor (type, mix, overdose, notes, sz) {
+        this.type = type
+        this.mix = mix
+        this.overdose = overdose
+        this.notes = notes
+        this.sz = sz
     }
-    const iAlcohol = new Gear('Alcohol&sup1','Mix 9#. Liquid. Antibiotic or Fuel. Unstable.',1);
-    const iAntibiotic = new Gear('Antibiotic*','Mix 12#. Prevents infection for Recovery 1/day.',0);
-    const iChloroform = new Gear('Chloroform&sup1','Mix 15#. Liquid. C#12 or Unconscious. Takes d6rnds.',0);
-    const iCyanide = new Gear('Cyanide&sup1*','Mix 18#. Pill. d6 Torso DMG/rnd for 5rnds.',0);
-    const iEpinephrine = new Gear('Epinephrine&sup1','Mix 15#. Injection. Rescuscitate within C+3mins.',0);
-    const iHallucinogen = new Gear('Hallucinogen*','Mix 15#. +1 Entertain and Tame. -3 all else. -1 Psyche.',0);
-    const iIodine = new Gear('Iodine*','Mix 6#. Purify 1gal of Water. Prevents Radiation.',0);
-    const iPainkiller = new Gear('Painkiller&sup1*','Mix 9#. Reduce Pain by 1.',0);
-    const iPotassiumChloride = new Gear('Potassium Chloride&sup1','Injection. d6 Torso DMG/min for 5mins.',0);
-    const iSedative = new Gear('Sedative&sup1*','Mix 12#. D#6/rnd to move. Prevents PSYCHOTIC BREAK.',0);
-    const iSodiumPentothal = new Gear('Sodium Pentothal&sup1','Mix 15#. Injection. -6 Entertain(Lie).',0);
-    const iStimulant = new Gear('Stimulant&sup1*','Mix 9#. Reduce Exhaustion by 1.',0);
-    const iDrugsList = [iAlcohol,iAntibiotic,iChloroform,iCyanide,iEpinephrine,iHallucinogen,iIodine,iPainkiller,iPotassiumChloride,iSedative,iSodiumPentothal,iStimulant];
+}
+
+export const DrugList = [
+    new Gear('Alcohol',             9,  True,   'Antibiotic or Fuel. C9# or Unstable.',                 1),
+    new Gear('Antibiotic',          12, False,  'Prevents infection in Recovery for 1 day.',            0),
+    new Gear('Hallucinogen',        15, False,  '+1 Perform and Tame. -3 all other rolls. -1 Psyche.',  0),
+    new Gear('Painkiller',          9,  True,   'Ignore 1 Pain penalty.',                               0),
+    new Gear('Sedative',            12, True,   'D#6/rnd to take any action.',                          0),
+    new Gear('Stimulant',           9,  True,   'Ignore Exhaustion penalties for 6hrs.',                0)
+]
+
+// OLD
+// new Gear('Chloroform',          15, True,   'Liquid. C#12 or Unconscious. Takes d6rnds.',       0)
+// new Gear('Cyanide',             18, True,   'Pill. d6 Torso DMG/rnd for 5rnds.',                0)
+// new Gear('Epinephrine',         15, True,   'Injection. Rescuscitate within C+3mins.',          0)
+// new Gear('Iodine',              6,  False,  'Purify 1gal of Water. Prevents Radiation.',        0)
+// new Gear('Potassium Chloride',  18, True,   'Injection. d6 Torso DMG/min for 5mins.',           0)
+// new Gear('Sodium Pentothal',    15, True,   'Injection. -6 Entertain(Lie).',                    0)
