17599b96e9b3891733f3d0d3eabecaf378cfd852
user <info@apocalyptiarpg.com>
2019-12-05 17:47:59 -0500

    fixed CoverTable in a div

--- src/rules/situations/CoverTable.svelte
+++ src/rules/situations/CoverTable.svelte
@@ -1,38 +1,13 @@
-<table>
-    <tr>
-        <th>Material</th>
-        <th>DR</th>
-    </tr>
-    <tr>
-        <td>Drywall</td>
-        <td>0</td>
-    </tr>
-    <tr>
-        <td>Glass</td>
-        <td>1</td>
-    </tr>
-    <tr>
-        <td>Plywood</td>
-        <td>1</td>
-    </tr>
-    <tr>
-        <td>Hardwood</td>
-        <td>2</td>
-    </tr>
-    <tr>
-        <td>Sheet Metal</td>
-        <td>2</td>
-    </tr>
-    <tr>
-        <td>Brick</td>
-        <td>3</td>
-    </tr>
-    <tr>
-        <td>Concrete</td>
-        <td>4</td>
-    </tr>
-    <tr>
-        <td>Steel</td>
-        <td>5</td>
-    </tr>
-</table>
+<div>
+    <table>
+        <tr><th>Material</th><th>DR</th></tr>
+        <tr><td>Drywall</td><td>0</td></tr>
+        <tr><td>Glass</td><td>1</td></tr>
+        <tr><td>Plywood</td><td>1</td></tr>
+        <tr><td>Hardwood</td><td>2</td></tr>
+        <tr><td>Sheet Metal</td><td>2</td></tr>
+        <tr><td>Brick</td><td>3</td></tr>
+        <tr><td>Concrete</td><td>4</td></tr>
+        <tr><td>Steel</td><td>5</td></tr>
+    </table>
+</div>
