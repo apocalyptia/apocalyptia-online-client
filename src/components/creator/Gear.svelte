bfad2e0f9f4c8b5820230435c3099d71e1b3ed1b
apocalyptia <info@apocalyptiarpg.com>
2019-12-15 16:36:43 -0500

    fixed broken Gear imports

--- src/components/creator/Gear.svelte
+++ src/components/creator/Gear.svelte
@@ -1,16 +1,19 @@
 <script>
-	import { AccessoryList } from '../../rules/gear/Accessories'
-	import { AmmoList } from '../../rules/gear/Ammo'
-	import { ArmorList, RandomArmor } from '../../rules/gear/Armor'
-	import { BombList } from '../../rules/gear/Bombs'
-	import { DocumentList } from '../../rules/gear/Documents'
-	import { DrugsList } from '../../rules/gear/Drugs'
-	import { ElectronicsList } from '../../rules/gear/Electronics'
-	import { EquipmentList } from '../../rules/gear/Equipment'
-	import { MedicalList } from '../../rules/gear/Medical'
-	import { MeleeList } from '../../rules/gear/Melee'
-	import { RangedList } from '../../rules/gear/Ranged'
-	import { StorageList } from '../../rules/gear/Storage'
+	import {
+		AccessoryList,
+		AmmoList,
+		ArmorList, 
+		RandomArmor,
+		BombList,
+		DocumentList,
+		DrugsList,
+		ElectronicsList,
+		EquipmentList,
+		MedicalList,
+		MeleeList,
+		RangedList,
+		StorageList
+	} from '../../rules/Gear'
 	import { CharacterStore } from '../../rules/Stores'
 	let char
 	const unsubscribe = CharacterStore.subscribe(value => { char = value })
