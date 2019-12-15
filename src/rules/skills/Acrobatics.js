e6ce5438d5af28665312ebfd678555a5b2b05740
apocalyptia <info@apocalyptiarpg.com>
2019-12-15 15:20:39 -0500

    consolidated Skills.js

--- src/rules/skills/Acrobatics.js
+++
@@ -1,7 +0,0 @@
-import Skill from './Skill.js'
-import Specialty from './Specialty.js'
-
-export const Acrobatics = new Skill(`Acrobatics`,     `Gymnastic prowess.`,   `Agility`,  6,  [
-    new Specialty(`Dodge`, `Roll vs [MATK or Ranged(Throw)].`),
-    new Specialty(`Jump`, `Running Jump [Speed]. Vertical [Speed x 3"].`)
-])
