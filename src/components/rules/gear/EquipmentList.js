2b19b0b5b0f96e40e59ce95b867c0b357ed17863
apocalyptia <jfannin@protonmail.com>
2020-03-29 12:59:19 -0400

    reworked ZipTie description

--- src/components/rules/gear/EquipmentList.js
+++ src/components/rules/gear/EquipmentList.js
@@ -628,7 +628,8 @@ export const Wristwatch = new Equipment({
 export const ZipTie = new Equipment({
 	name: `Zip Tie`,
 	description: [
-		`Grabbed. C9# to escape. Use for +1 Build.`,
+		`Place on Arms behind target's back to make them Harmless. Place on Legs to make target Immobilized. Constitution or Acrobatics 12# to escape.`,
+		`Use for +1 Build.`,
 	],
 	sz: 0
 })
