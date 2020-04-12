aa6afabb6f25263d4abda670706a45f53cf5b19c
apocalyptia <jfannin@protonmail.com>
2020-04-12 00:04:40 -0400

    removed express. installed polka.

--- src/server.js
+++ src/server.js
@@ -1,17 +1,21 @@
 import sirv from 'sirv'
-import express from 'express'
+import polka from 'polka'
 import compression from 'compression'
 import * as sapper from '@sapper/server'
 
-const app = express()
 
 const { PORT, NODE_ENV } = process.env
 const dev = NODE_ENV === 'development'
 
-app.use(express.json())
 
-app.use(
-	compression({ threshold: 0 }),
-	sirv('static', { dev }),
-	sapper.middleware()
-).listen(PORT, err => { if (err) console.log('error', err) })
+polka()
+	.use(
+		compression({ threshold: 0 }),
+		sirv('static', { dev }),
+		sapper.middleware()
+	)
+	.listen(
+		PORT, err => {
+			if (err) console.log('error', err)
+		}
+	)
