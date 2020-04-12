f9d764a4b5cdb797ceb6dcc4e787d44e435140e5
apocalyptia <jfannin@protonmail.com>
2020-04-12 10:51:10 -0400

    character write to db 1st attempt

--- src/routes/creator/sheet.svelte
+++ src/routes/creator/sheet.svelte
@@ -2,13 +2,13 @@
 import { character } from '../../stores/characterStore'
 import CharacterSheet from '../../components/views/ui/CharacterSheet.svelte'
 import NavBar from '../../components/views/controls/NavBar.svelte'
-// import api from '../../../utils/api'
+import api from '../../../utils/api'
 
 $character.completed = true
 
 const createMyCharacter = (c) => {
-	// console.log(c)
-	// api.createCharacter(c)
+	console.log(c)
+	api.createCharacter(c)
 }
 </script>
 
