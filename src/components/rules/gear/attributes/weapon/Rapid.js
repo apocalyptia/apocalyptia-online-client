c41c73ecbef7a2db283008b8615a1ddc0f0ac5cc
Jordan Fannin <jfannin@protonmail.com>
2020-05-18 22:51:58 -0400

    made double-tap revision to Rapid

--- src/components/rules/gear/attributes/weapon/Rapid.js
+++ src/components/rules/gear/attributes/weapon/Rapid.js
@@ -4,7 +4,7 @@ import WeaponAttr from './WeaponAttr'
 const Rapid = new WeaponAttr({
 	name: `Rapid`,
 	desc: [
-		`2 Attacks for 1 Action.`,
+		`You can make 2 Attacks at the same target for 1 Action.`,
 	]
 })
 
