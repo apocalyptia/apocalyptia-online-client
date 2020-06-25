3f0da0345e1e61dcad6e95e8bbc7bf15b413f08c
Jordan Fannin <info@apocalyptiarpg.com>
2020-06-24 22:52:28 -0400

    add console logging for create character lambda

--- functions/character-create.js
+++ functions/character-create.js
@@ -5,6 +5,7 @@ const client = new faunadb.Client({ secret: process.env.FAUNADB_SERVER_SECRET })
 exports.handler = async (event) => {
 	const data = JSON.parse(event.body)
 	const characterItem = { data: data }
+	console.log('Creating a new character.')
 	return client.query(
 		q.Create(
 			q.Collection('characters'),
@@ -12,11 +13,13 @@ exports.handler = async (event) => {
 		)
 	)
 	.then(res => {
+		console.log('New character created successfully.')
 		return {
 			statusCode: 200,
 			body: JSON.stringify(res)
 		}
 	}).catch(err => {
+		console.log('Failed to create new character.')
 		return {
 			statusCode: 400,
 			body: JSON.stringify(err)
