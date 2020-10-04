883c67caff3da6923cb41ab0aad21a3d87ba6fc6
user <user@pop-os.localdomain>
2020-10-03 21:49:28 -0400

    ref back button

---
+++ src/views/widgets/BackButton.svelte
@@ -0,0 +1,31 @@
+<script>
+    export let path
+</script>
+
+
+<a href={ path } class='back-btn'>
+	<div class='back-btn'>&#9668;</div>
+</a>
+
+
+<style>
+    .back-btn {
+		align-items: center;
+		border: 2px solid lime;
+		bottom: 0;
+		display: flex;
+		font-size: var(--s150);
+		height: 46px;
+		justify-content: center;
+		left: 0;
+		position: fixed;
+		text-align: center;
+		width: 46px;
+	}
+	.back-btn:active,
+	.back-btn:focus,
+	.back-btn:hover {
+		background-color: lime;
+		color: rgba(15, 30, 15, 1);
+	}
+</style>
