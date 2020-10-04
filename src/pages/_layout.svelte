ddf087b5d558b55d9fdbc1085e4d2b6df582f8b8
user <user@pop-os.localdomain>
2020-10-03 21:49:19 -0400

    display window changes

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,13 +1,34 @@
 <script>
 	import TitleBar from '../views/widgets/TitleBar.svelte'
-	import DisplayWindow from '../views/widgets/DisplayWindow.svelte'
 	import { version } from '../main'
 </script>
 
 
 <main>
-	<TitleBar {version}/>
-	<DisplayWindow>
-		<slot/>
-	</DisplayWindow>
+	<TitleBar {version} />
+	<div class='display-window'>
+		<div class='page-container'>
+			<slot/>
+		</div>
+	</div>
 </main>
+
+
+<style>
+	::-webkit-scrollbar {
+		display: none;
+	}
+	.display-window {
+		height: calc(100vh - var(--s300));
+		overflow: scroll;
+		position: absolute;
+		scrollbar-width: none;
+		top: var(--s300);
+		width: 100vw;
+	}
+	.page-container {
+		overflow: scroll;
+		padding: var(--s100);
+		scrollbar-width: none;
+	}
+</style>
