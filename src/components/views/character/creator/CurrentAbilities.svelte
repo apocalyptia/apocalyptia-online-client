dc72d473163e26d4be3a779608923af2d952ed87
user <user@pop-os.localdomain>
2020-11-29 19:29:24 -0500

    fixed ability nbsp

--- src/components/views/character/creator/CurrentAbilities.svelte
+++ src/components/views/character/creator/CurrentAbilities.svelte
@@ -23,10 +23,7 @@
 			{#each abilities as ability}
 				<div class='current-ability-row'>
 					<span class='l-col'>
-						{ability.name}
-						{#if ability.opts[0]}
-							&nbsp;({ability.opts[0].name})
-						{/if}
+						{ability.name}{ability.opts[0] ? ` (${ability.opts[0].name})` : ``}
 					</span>
 					<span class='s-col'>{ability.xp}</span>
 					<span class='s-col'>{ability.max}</span>
