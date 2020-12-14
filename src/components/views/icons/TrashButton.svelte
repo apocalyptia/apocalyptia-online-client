3d0f4a8910e380e632efd82b8f63b9875354f7d3
user <user@pop-os.localdomain>
2020-12-13 23:52:28 -0500

    improved trash btn ui

--- src/components/views/icons/TrashButton.svelte
+++ src/components/views/icons/TrashButton.svelte
@@ -4,31 +4,14 @@
 </script>
 
 
-<button class='btn-box trash-btn' on:click={_ => deleteFunction(args)}>
+<button class='btn-box trash-btn crimson-btn' on:click={_ => deleteFunction(args)}>
 	<div class='btn-icon'>X</div>
 </button>
 
 
 <style>
 	.trash-btn {
-		-webkit-text-fill-color: var(--sec-color);
-		background-color: var(--ter-color);
-		border: 1px solid var(--sec-color);
-		color: var(--sec-color);
-		font-size: var(--s150);
-		font-weight: bold;
 		height: var(--square);
-		max-height: var(--square);
-		max-width: var(--square);
-		min-height: var(--square);
-		min-width: var(--square);
 		width: var(--square);
-	}
-	.trash-btn:hover {
-		-webkit-text-fill-color: var(--ter-color);
-		background-color: var(--sec-color);
-		border: 1px solid var(--ter-color);
-		box-shadow: 0 0 15px 5px var(--ter-color-trans);
-		color: var(--ter-color);
 	}
 </style>
