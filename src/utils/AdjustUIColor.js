b3a253eb509b72f296a2b213fadb6900e035c85d
user <user@pop-os.localdomain>
2020-12-13 23:15:32 -0500

    fixed color bug

--- src/utils/AdjustUIColor.js
+++ src/utils/AdjustUIColor.js
@@ -9,25 +9,25 @@ export default (character) => {
 
 	const health = current / max
 
-	let pR = (health < 1) ? (1.5-health)*255 : 0
-	let pG = (health < 1) ? (1.5*health)*255 : 255
+	let pR = (health < 1) ? (1.5 - health) * 255 : 0
+	let pG = (health < 1) ? (1.5 * health) * 255 : 255
 	let pB = 0
 
-	let sR = (health < 1) ? (1.5-health)*30 : 15
-	let sG = (health < 1) ? (1.5*health)*30 : 30
+	let sR = (health < 1) ? (1.5 - health) * 30 : 15
+	let sG = (health < 1) ? (1.5 * health) * 30 : 30
 	let sB = 15
 
 	if ((headCurrent < 1) || (torsoCurrent < 1)) {
-		pR = (health*64)+192
-		pG = (health*64)+192
-		pB = (health*64)+192
+		pR = (health * 64) + 192
+		pG = (health * 64) + 192
+		pB = (health * 64) + 192
 		sR = 0
 		sG = 0
 		sB = 0
 	}
 
-	document.documentElement.style.setProperty(`--pri-color`, `rgba(${pR},${pG},${pB},1)`)
-	document.documentElement.style.setProperty(`--pri-color-trans`, `rgba(${pR},${pG},${pB},.25)`)
-	document.documentElement.style.setProperty(`--sec-color`, `rgba(${sR},${sG},${sB},1)`)
-	document.documentElement.style.setProperty(`--sec-color-trans`, `rgba(${sR},${sG},${sB},.25)`)
+	document.documentElement.style.setProperty(`--pri-color`, `rgba(${pR}, ${pG}, ${pB}, .9)`)
+	document.documentElement.style.setProperty(`--pri-color-trans`, `rgba(${pR}, ${pG}, ${pB}, .5)`)
+	document.documentElement.style.setProperty(`--sec-color`, `rgba(${sR}, ${sG}, ${sB}, .85)`)
+	document.documentElement.style.setProperty(`--sec-color-trans`, `rgba(${sR}, ${sG}, ${sB}, .5)`)
 }
