3a988cdea6def9237506c600c9e5a7ca156842d7
Jordan Fannin <info@apocalyptiarpg.com>
2021-05-24 22:26:04 -0400

    moved .git dir to proper location

---
+++ svelte.config.js
@@ -0,0 +1,12 @@
+const netlify = require('@sveltejs/adapter-netlify')
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	kit: {
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte',
+		adapter: netlify()
+	}
+};
+
+export default config
