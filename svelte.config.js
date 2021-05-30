8032433a12f0b72085ac989ec120ccdec4b9c40c
Jordan Fannin <info@apocalyptiarpg.com>
2021-05-30 01:18:46 -0400

    added netlify build option

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
-import node from '@sveltejs/adapter-node'
+// import node from '@sveltejs/adapter-node'
+import netlify from '@sveltejs/adapter-netlify'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		adapter: node()
+		// adapter: node()
+		adapter: netlify()
 	}
 };
 
