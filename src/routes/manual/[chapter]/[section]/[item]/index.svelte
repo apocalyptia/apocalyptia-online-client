3577b122993b23df37286a66fc3f99be108785e2
Jordan Fannin <info@apocalyptiarpg.com>
2021-05-30 00:55:03 -0400

    restored manual search

--- src/routes/manual/[chapter]/[section]/[item]/index.svelte
+++ src/routes/manual/[chapter]/[section]/[item]/index.svelte
@@ -19,6 +19,11 @@
 	export let chapter, section, item
 
 	const rule = Rules[chapter][section][item]
+
+	console.log('chapter = ', chapter)
+	console.log('section = ', section)
+	console.log('item = ', item)
+	console.log('Rules[chapter][section][item] = ', Rules[chapter][section][item])
 </script>
 
 
--- src/utils/searching/searchEngine.js
+++ src/utils/searching/searchEngine.js
@@ -1,33 +1,55 @@
+import Rules from '/src/rules/Rules.js'
 import alphabetize from '/src/utils/sorting/alphabetize.js'
+import capitalize from '/src/utils/text/capitalize.js'
+import swapOrder from '/src/utils/text/swapOrder.js'
+import urlFormat from '/src/utils/text/urlFormat.js'
 
-function searchEngine(searchTerm, searchList) {
-
-	const mappedList = []
-
-	function addSublists(rule) {
-		if (rule.hasOwnProperty('list')) addSublists(rule.list)
-		else mappedList.push(rule)
-	}
-
-	addSublists(searchList)
-
-	const sortedRulesList = alphabetize(mappedList)
-
-	let ruleList = sortedRulesList
+function searchEngine(searchTerm='') {
+	let displayList = []
 
 	if (searchTerm.length) {
-		ruleList = ruleList.filter(r => !r.hasOwnProperty('list'))
-		ruleList = sortedRulesList.filter(r => {
-			return r.name.toLocaleLowerCase()
-						.startsWith(searchTerm.toLocaleLowerCase())
+
+		const chapterList = Object.entries(Rules).map(chapter => {
+			return {
+				name: capitalize(chapter[0]),
+				content: chapter[1],
+				url: urlFormat(`/manual/${chapter[0]}`)
+			}
 		})
-		if (!ruleList.length) {
-			ruleList = sortedRulesList.filter(r => {
-				return r.name.toLocaleLowerCase()
-							.includes(searchTerm.toLocaleLowerCase())
+
+		const resultsList = alphabetize(
+			chapterList.flatMap(chapter => {
+				return Object.entries(chapter.content).flatMap(section => {
+					if (section[1].hasOwnProperty('name')) {
+						section[1].url = urlFormat(`/manual/${chapter.name}/${section[1].name}`)
+						return section[1]
+					}
+					else {
+						return Object.values(section[1]).flatMap(item => {
+							item.url = urlFormat(`/manual/${chapter.name}/${section[0]}/${section[0] === 'ammo' ? swapOrder(item.name) : item.name}`)
+							console.log(item.url)
+							return item
+						})
+					}
+				})
+			}).concat(chapterList)
+		)
+
+		displayList = resultsList.filter(r => {
+			return r.name.toLowerCase()
+						.startsWith(searchTerm.toLowerCase())
+		})
+	
+		if (!displayList.length) {
+			displayList = resultsList.filter(r => {
+				return r.name.toLowerCase()
+							.includes(searchTerm.toLowerCase())
 			})
 		}
+
 	}
 
-	return ruleList
+	return displayList
 }
+
+export default searchEngine
