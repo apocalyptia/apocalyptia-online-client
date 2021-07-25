dd4b201812bb7dbfebdcb54cc7dadacbeedf034a
Jordan Fannin <info@apocalyptiarpg.com>
2021-07-25 15:04:35 -0400

    new formatting and linting

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,14 @@
-// import node from '@sveltejs/adapter-node'
-import netlify from '@sveltejs/adapter-netlify'
+import adapter from '@sveltejs/adapter-netlify'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		// adapter: node()
-		adapter: netlify()
-	}
+		adapter: adapter(),
+		files: {
+			assets: 'static',
+		},
+	},
 }
 
 export default config
