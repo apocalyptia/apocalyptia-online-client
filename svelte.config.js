d2e5062fd6dc0d40d66a226445240a12099cd414
Jordan Fannin <info@apocalyptiarpg.com>
2022-02-17 19:57:13 -0500

    svelte version update

--- svelte.config.js
+++ svelte.config.js
@@ -3,7 +3,6 @@ import adapter from '@sveltejs/adapter-netlify'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		target: '#svelte',
 		adapter: adapter(),
 		files: {
 			assets: 'static',
